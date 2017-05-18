1e844a3be8ed4b9f8ec2543587455feacc7c96b2
veranyc <veranyc@gmail.com>
2017-05-18 09:55:37 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,36 @@
 require 'pry'
 
 class String
+  attr_reader :string
 
   def sentence?
-    
+    #if the string called ends in a "." then return true
+    #else return false
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    self.split(/\? |\. |\! /).length
   end
 end
